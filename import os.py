6c1d87b5e9716ed9846ff3d1d9d9cd7ca0d67967
Toby_csc <15141566183@139.com>
2025-06-15 00:22:07 +0800

    fix：更新测试脚本

--- import os.py
+++ import os.py
@@ -16,23 +16,26 @@ s = serial.Serial('COM8', 115200, timeout=0.5,write_timeout=0.1)
 head = '72 7d'
 tail = '7e 7d'
 
-sendcount = 5000
+sendcount = 2000
 
             
 def replay():
     global sendcount
-    while(sendcount < 15001):
+    while(sendcount < 4001):
         #data = head + ' ' + hex(sendcount)[2:]
-        data = head + ' ' + hex(sendcount)[4:] + ' ' + hex(sendcount)[2:4] + ' ' + hex(sendcount)[4:]+ ' ' + hex(sendcount)[2:4]  + ' ' + tail
+        data = head + ' ' + hex(sendcount)[3:] + ' 0' + hex(sendcount)[2:3] + ' ' + hex(sendcount)[3:]+ ' 0' + hex(sendcount)[2:3]  + ' ' + tail
+
+        print(sendcount)
+        print(hex(sendcount))
         print(data)
         try:
             s.write(bytes.fromhex(data))
         except serial.SerialTimeoutException:
             print("bruh")
         sendcount = sendcount + 10
-        if(sendcount == 15000):
-            sendcount = 5000
-        time.sleep(0.005)
+        if(sendcount == 4000):
+            sendcount = 2000
+        time.sleep(0.002)
         
 
 
